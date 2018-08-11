61ab59446baf3ad3795bc606535c9cae0f1b9c20
matthexx <matthew_imadiyi@yahoo.com>
2018-08-11 12:23:10 +0100

    Initial commit--Empty mobile module.

---
+++ tv/src/main/java/ng/com/tingomobile/tingo/TvSplashActivity.java
@@ -0,0 +1,13 @@
+package ng.com.tingomobile.tingo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TvSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv_splash);
+    }
+}
